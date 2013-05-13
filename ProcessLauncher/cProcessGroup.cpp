95e3b070dabc31aebc4a7ef72f41e331aae00a61
Calvin De Lima <calvindlm@gmail.com>
2013-05-12 22:56:36 -0700

    Started implementing ``LaunchProcessGroup``.

--- ProcessLauncher/cProcessGroup.cpp
+++ ProcessLauncher/cProcessGroup.cpp
@@ -13,5 +13,26 @@ ProcessGroup::ProcessGroup( std::list< Process::proc_ptr > grp ) {
   processes in the group return.
  */
 void ProcessGroup::LaunchProcessGroup() {
+	DWORD nCount = _lst.size();
+	HANDLE *lpHandles = NULL;
+	int idx = -1;
+
+	// TODO: Iterate over list of ``Process`` objects and launch them then
+	//       wait for their created handles using `GetHandle` method.
+
+	// for each process:
+		// 1. Launch
+
+		// 2. GetProcess()
+
+		// 3. Block until the handles are signaled.
+		DWORD ret = WaitForMultipleObjects(
+		  nCount,		// _In_  DWORD nCount
+		  lpHandles,	// _In_  const HANDLE *lpHandles
+		  true,			// _In_  BOOL bWaitAll
+		  0				// _In_  DWORD dwMilliseconds
+		);
+
 
 }
+
