bfde43e881457ecb1717c23c732eb044f85eed01
Calvin De Lima <calvindlm@gmail.com>
2013-05-16 08:09:16 -0400

    Waits on multiple objects, need to add more to ``infile.txt``.

--- ProcessLauncher/cProcessGroup.cpp
+++ ProcessLauncher/cProcessGroup.cpp
@@ -15,27 +15,30 @@ ProcessGroup::ProcessGroup( std::list< Process::proc_ptr > grp ) {
  */
 void ProcessGroup::LaunchProcessGroup() {
 	DWORD nCount = _lst.size();
-	HANDLE *lpHandles = NULL;
+	HANDLE lpHandles[256] = { 0 };
 	int idx = -1;
 
 	// TODO: Iterate over list of ``Process`` objects and launch them then
 	//       wait for their created handles using `GetHandle` method.
 
 	std::list< Process::proc_ptr >::iterator it = _lst.begin();
+
 	for( ; it != _lst.end(); ++it )
 	{
 		// Launch the process
-		it->get()->RunProcess();
+		if( it->get()->RunProcess() != 0) {
+			// TODO: Print to std err.
+		}
 
 		// 2. GetProcess()
+		lpHandles[ ++idx ] = it->get()->GetProcessHandle();
 	}
 
 	// 3. Block until the handles are signaled.
-	DWORD ret = WaitForMultipleObjects(
-		nCount,		// _In_  DWORD nCount
+	DWORD ret = WaitForMultipleObjects(nCount,// _In_  DWORD nCount
 		lpHandles,	// _In_  const HANDLE *lpHandles
-		true,			// _In_  BOOL bWaitAll
-		0				// _In_  DWORD dwMilliseconds
+		true,		// _In_  BOOL bWaitAll
+		INFINITE	// _In_  DWORD dwMilliseconds
 	);
 
 }
